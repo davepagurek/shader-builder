4c5ff87db9458a5e7fb82d951f1277d96a47a0ce
Dave Pagurek <davepagurek@gmail.com>
2023-09-17 16:53:41 -0400

    Working connections

--- shader-builder.ts
+++ shader-builder.ts
@@ -2,7 +2,19 @@ import { DeclarationNode, DeclarationStatementNode, DeclaratorListNode, Function
 import { parse } from '@shaderfrog/glsl-parser/parser/parser'
 import { Scope } from '@shaderfrog/glsl-parser/parser/scope'
 
-class ShaderSnippet {
+interface ShaderContext {
+  type: 'vert' | 'frag'
+  version: 100 | 300
+}
+
+interface ShaderGraphNode {
+  reference(ctx: ShaderContext): string
+  declaration(ctx: ShaderContext): string
+  header(ctx: ShaderContext): string
+  dependsUpon(ctx: ShaderContext): ShaderGraphNode[]
+}
+
+export class ShaderSnippet implements ShaderGraphNode {
   program: Program
   globalScope: Scope
   mainFunctionName: string
@@ -12,9 +24,11 @@ class ShaderSnippet {
   outputType: string
   outputProperties: { [name: string]: string } | null
 
-  constructor(snippet: string, { mainFunction: rawMainFunction }: { mainFunction?: string } = {}) {
+  uses: number = 0
+
+  constructor(public snippet: string, { mainFunction: rawMainFunction }: { mainFunction?: string } = {}) {
     this.program = parse(snippet)
-    this.globalScope = this.program.scopes.find((scope) => scope.name === 'global')
+    this.globalScope = this.program.scopes.find((scope) => scope?.name === 'global')!
     const mainFunctionName = rawMainFunction ?? last(Object.keys(this.globalScope.functions))
     const mainFunctionSignatures = this.globalScope.functions[mainFunctionName]
     const mainFunction = last(Object.values(mainFunctionSignatures))
@@ -28,7 +42,7 @@ class ShaderSnippet {
     this.outputType = mainFunction.returnType
     const outputTypeNode = this.globalScope.types[this.outputType]
     if (outputTypeNode) {
-      this.outputProperties = {}
+      const outputProperties: { [name: string]: string } = {}
       const typeDeclaration = this.program.program.find(
         (node) => (node as any).declaration?.specified_type?.specifier?.specifier?.typeName?.identifier === this.outputType
       )
@@ -38,12 +52,30 @@ class ShaderSnippet {
       for (const declaration of (typeDeclaration as any).declaration.specified_type.specifier.specifier.declarations as StructDeclarationNode[]) {
         const typeName = (declaration.declaration.specified_type.specifier.specifier as KeywordNode).token
         for (const propNode of declaration.declaration.declarations) {
-          this.outputProperties[propNode.identifier.identifier] = typeName
+          outputProperties[propNode.identifier.identifier] = typeName
         }
       }
+      this.outputProperties = outputProperties
     } else {
       this.outputProperties = null
     }
+  }
+
+  reference() {
+    return ''
+  }
+
+  declaration() {
+    return ''
+  }
+
+  dependsUpon() {
+    return []
+  }
+
+  header() {
+    // TODO: rename types
+    return this.snippet
   }
 
   instantiate() {
@@ -51,57 +83,376 @@ class ShaderSnippet {
   }
 }
 
-type Input = { node: ShaderSnippetInstance, output: string | null }
+interface ShaderValue extends ShaderGraphNode {
+  typeName(): string
+}
 
-class ShaderSnippetInstance {
-  inputs: { [name: string]: Input | null }
+export class ShaderSnippetInstance implements ShaderGraphNode {
+  inputs: { [name: string]: ShaderSnippetInstanceInput }
+  output: ShaderSnippetInstanceOutput | { [name: string]: ShaderSnippetInstanceOutput }
+  id: string
+
   constructor(public snippet: ShaderSnippet) {
+    this.id = `__${this.snippet.mainFunctionName}_out_${this.snippet.uses++}`
     this.inputs = {}
     for (const input of snippet.inputs) {
-      this.inputs[input] = null
+      this.inputs[input] = new ShaderSnippetInstanceInput(this, input)
+    }
+    if (this.snippet.outputProperties) {
+      this.output = {}
+      for (const prop in this.snippet.outputProperties) {
+        this.output[prop] = new ShaderSnippetInstanceOutput(this, prop)
+      }
+    } else {
+      this.output = new ShaderSnippetInstanceOutput(this, null)
     }
   }
 
+  reference() {
+    return this.id
+  }
+
+  declaration(ctx: ShaderContext) {
+    return `${this.id} = ${this.snippet.mainFunctionName}(${
+      this.snippet.inputs.map((key) => this.inputs[key].reference(ctx)).join(', ')
+    });`
+  }
+
+  header() {
+    return ''
+  }
+
+  dependsUpon(): ShaderGraphNode[] {
+    if (!this.isComplete()) {
+      throw new Error(
+        `This snippet doesn't have complete inputs: ${
+          Object.keys(this.inputs).filter((k) => !this.inputs[k].input).join(', ')
+        }`
+      )
+    }
+    return [this.snippet, ...Object.values(this.inputs)]
+  }
+
   isComplete() {
-    for (const key in this.inputs) {
-      if (this.inputs[key] === null) return false
+    for (const input of Object.values(this.inputs)) {
+      if (input.input === null) return false
     }
     return true
   }
 
   connect(property: string | null = null): ShaderSnippetInstanceOutput {
-    return new ShaderSnippetInstanceOutput(this, property)
+    if (property === null) {
+      if (!(this.output instanceof ShaderSnippetInstanceOutput)) {
+        throw new Error('Please pass a property name to connect(), since it is a struct')
+      }
+      return this.output
+    } else {
+      if (this.output instanceof ShaderSnippetInstanceOutput) {
+        throw new Error('Cannot pass a property name to connect() with a single value output')
+      }
+      return this.output[property]
+    }
+  }
+
+  connectTo(input: Inputtable) {
+    return this.connect().to(input)
   }
 }
 
-class ShaderSnippetInstanceInput {
-  constructor(public source: ShaderSnippetInstance, public property: string) {}
-
-  typeName(): string {
-    return this.source.snippet.inputs[this.property]
+export abstract class Inputtable {
+  _input: ShaderValue | null = null
+  get input() {
+    if (this._input === null) {
+      throw new Error('Input is not connected!')
+    }
+    return this._input
   }
 
-  attach(output: ShaderSnippetInstanceOutput) {
+  abstract typeName(): string
+
+  attach(output: ShaderValue) {
     if (this.typeName() !== output.typeName()) {
       throw new Error(`Could not connect nodes: output type ${output.typeName()} does not match input type ${this.typeName()}`)
     }
-    this.source.inputs[this.property] = { node: output.source, output: output.property }
+    this._input = output
   }
 }
 
-class ShaderSnippetInstanceOutput {
+export class ShaderSnippetInstanceInput extends Inputtable implements ShaderGraphNode {
+  constructor(public source: ShaderSnippetInstance, public property: string) {
+    super()
+  }
+
+  reference(ctx: ShaderContext) {
+    if (this.input === null) {
+      throw new Error(`Input ${this.property} of ${this.source.reference()} is not connected!`)
+    }
+    return this.input.reference(ctx)
+  }
+
+  header() {
+    return ''
+  }
+
+  declaration() {
+    return ''
+  }
+
+  dependsUpon() {
+    return [this.source, this.input]
+  }
+
+  typeName(): string {
+    return this.source.snippet.inputTypes[this.property]
+  }
+}
+
+export class DefaultShaderOutput extends Inputtable implements ShaderGraphNode {
+  constructor(public type: string, public name: string) {
+    super()
+  }
+
+  typeName() {
+    return this.type
+  }
+
+  reference() {
+    return this.name
+  }
+
+  declaration(ctx: ShaderContext) {
+    return `${this.name} = ${this.input.reference(ctx)};`
+  }
+
+  header() {
+    return ''
+  }
+
+  dependsUpon() {
+    return [this.input]
+  }
+}
+
+export class DefaultShaderInput implements ShaderValue {
+  constructor(public type: string, public name: string) {}
+
+  reference() {
+    return this.name
+  }
+
+  declaration() {
+    return ''
+  }
+
+  dependsUpon() {
+    return []
+  }
+
+  typeName() {
+    return this.type
+  }
+
+  header() {
+    return ''
+  }
+}
+
+export class ShaderAttribute implements ShaderValue {
+  constructor(public type: string, public name: string) {}
+
+  reference(ctx: ShaderContext) {
+    if (ctx.type === 'frag') {
+      throw new Error(`Can't reference attribute ${this.name} from a fragment shader. Please add a varying in between.`)
+    }
+    return this.name
+  }
+
+  declaration() {
+    return ''
+  }
+
+  dependsUpon() {
+    return []
+  }
+
+  header() {
+    return `attribute ${this.type} ${this.name};`
+  }
+
+  typeName(): string {
+    return this.type
+  }
+
+  connectTo(input: Inputtable) {
+    return input.attach(this)
+  }
+}
+
+export class ShaderVarying extends Inputtable implements ShaderValue {
+  constructor(public type: string, public name: string) {
+    super()
+  }
+
+  reference() {
+    return this.name
+  }
+
+  declaration(ctx: ShaderContext) {
+    if (ctx.type === 'vert') {
+      return `${this.name} = ${this.input.reference(ctx)}`
+    } else {
+      return ''
+    }
+  }
+
+  dependsUpon(ctx: ShaderContext) {
+    if (ctx.type === 'vert') {
+      return [this.input]
+    } else {
+      return []
+    }
+  }
+
+  header() {
+    return `varying ${this.type} ${this.name};`
+  }
+
+  typeName(): string {
+    return this.type
+  }
+
+  connectTo(input: Inputtable) {
+    return input.attach(this)
+  }
+}
+
+export class ShaderUniform implements ShaderValue {
+  constructor(public type: string, public name: string) {}
+
+  header() {
+    return `uniform ${this.type} ${this.name};`
+  }
+
+  declaration() {
+    return ''
+  }
+
+  reference() {
+    return this.name
+  }
+
+  dependsUpon() {
+    return []
+  }
+
+  typeName(): string {
+    return this.type
+  }
+
+  connectTo(input: Inputtable) {
+    return input.attach(this)
+  }
+}
+
+export class ShaderSnippetInstanceOutput implements ShaderValue {
   constructor(public source: ShaderSnippetInstance, public property: string | null) {}
+
+  reference() {
+    if (this.property === null) {
+      return this.source.reference()
+    } else {
+      return `${this.source.reference()}.${this.property}`
+    }
+  }
+
+  declaration() {
+    return ''
+  }
+
+  header() {
+    return ''
+  }
+
+  dependsUpon() {
+    return [this.source]
+  }
 
   typeName() {
     if (this.property) {
-      return this.source.snippet.outputProperties[this.property]
+      return this.source.snippet.outputProperties![this.property]
     } else {
       return this.source.snippet.outputType
     }
   }
 
-  to(input: ShaderSnippetInstanceInput) {
+  to(input: Inputtable) {
     input.attach(this)
+  }
+}
+
+export type Precision = 'lowp' | 'mediump' | 'highp'
+export class ShaderGraph {
+  position = new DefaultShaderOutput('vec4', 'gl_Position')
+  fragPosition = new DefaultShaderInput('vec4', 'gl_FragPosition')
+  color = new DefaultShaderOutput('vec4', 'gl_FragColor')
+  intPrecision: Precision
+  floatPrecision: Precision
+  version: 100 | 300
+
+  constructor({
+    intPrecision = 'highp',
+    floatPrecision = 'highp',
+    version = 100,
+  }: {
+    intPrecision?: Precision
+    floatPrecision?: Precision
+    version?: 100 | 300
+  } = {}) {
+    this.intPrecision = intPrecision
+    this.floatPrecision = floatPrecision
+    this.version = version
+  }
+
+  build() {
+    const vert = this.buildShader(this.position, { type: 'vert', version: this.version })
+    const frag = this.buildShader(this.color, { type: 'frag', version: this.version })
+    return { vert, frag }
+  }
+
+  private buildShader(output: DefaultShaderOutput, ctx: ShaderContext) {
+    const nodes = new Set<ShaderGraphNode>()
+    const orderedNodes: ShaderGraphNode[] = []
+    const mainSource: string[] = []
+    const headerSource: string[] = [`precision ${this.intPrecision} int;`, `precision ${this.floatPrecision} float;`]
+    if (ctx.version === 300) {
+      headerSource.unshift('#version 300 es')
+    }
+
+    function processNode(node: ShaderGraphNode) {
+      if (nodes.has(node)) return
+      nodes.add(node)
+      orderedNodes.unshift(node)
+      for (const dependency of node.dependsUpon(ctx)) {
+        processNode(dependency)
+      }
+    }
+    processNode(output)
+
+    for (const node of orderedNodes) {
+      const header = node.header(ctx)
+      if (header) {
+        headerSource.push(header)
+      }
+      const declaration = node.declaration(ctx)
+      if (declaration) {
+        mainSource.push(declaration)
+      }
+    }
+    const src = `${headerSource.join('\n')}
+void main() {
+${mainSource.join('\n')}
+}`
+    return { nodes, src }
   }
 }
 
---
+++ test/test.ts
@@ -0,0 +1,30 @@
+import { ShaderAttribute, ShaderGraph, ShaderSnippet, ShaderUniform, ShaderVarying } from '../shader-builder'
+
+const graph = new ShaderGraph()
+
+const p5Inputs = {
+  position: new ShaderAttribute('vec3', 'aPosition'),
+  texCoord: new ShaderAttribute('vec2', 'aTexCoord'),
+  vertexColor: new ShaderAttribute('vec4', 'aVertexColor'),
+  mvMatrix: new ShaderUniform('mat4', 'uModelViewMatrix'),
+  projMatrix: new ShaderUniform('mat4', 'uProjectionMatrix'),
+}
+
+const ApplyCamera = new ShaderSnippet(`
+  vec4 applyCamera(mat4 mvMatrix, mat4 projMatrix, vec3 worldPos) {
+    return mvMatrix * projMatrix * worldPos;
+  }
+`)
+const applyCamera = ApplyCamera.instantiate()
+p5Inputs.position.connectTo(applyCamera.inputs.worldPos)
+p5Inputs.mvMatrix.connectTo(applyCamera.inputs.mvMatrix)
+p5Inputs.projMatrix.connectTo(applyCamera.inputs.projMatrix)
+applyCamera.connectTo(graph.position)
+
+const varyingColor = new ShaderVarying('vec4', 'vVertexColor')
+p5Inputs.vertexColor.connectTo(varyingColor)
+varyingColor.connectTo(graph.color)
+
+const { vert, frag } = graph.build()
+console.log(vert)
+console.log(frag)
